fd3166a6add35cd754cbc7be13dca7513933ad79
stefan0303 <tepsiev@hotmail.com>
2017-09-11 12:12:21 +0300

    C# Basics Draw Figures

---
+++ C# Basics/Drawing Figures with Loops on Console/09.Eclipse/03.Eclipse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+  class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine()); //Write a number
+            Console.WriteLine(" {0}{1}{0}", new string('*', n * 2-2), new string(' ', n+1));
+            for (int i = 1; i < n - i - 1; i++)
+            {
+                Console.WriteLine("*{0}*{1}*{0}*", new string('/', n * 2 - 2), new string(' ', n-1));
+            }
+            Console.WriteLine("*{0}*{1}*{0}*", new string('/', n * 2 - 2), new string('-', n-1));
+            for (int i = 1; i < n - i - 1; i++)
+            {
+                Console.WriteLine("*{0}*{1}*{0}*", new string('/', n * 2 - 2), new string(' ', n-1));
+            }
+            Console.WriteLine(" {0}{1}{0}", new string('*', n * 2-2), new string(' ', n+1));
+        }
+    }
+
---
+++ C# Basics/Drawing Figures with Loops on Console/10.Sunglasses/10.Sunglasses.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine()); //
+            Console.WriteLine("{0}{1}{0}",new string('*',n*2),new string(' ',n));
+            for (int i = 1; i < n-i-1;i++ )
+            {
+                Console.WriteLine("*{0}*{1}*{0}*",new string('/',n*2-2),new string (' ',n));
+            }
+            Console.WriteLine("*{0}*{1}*{0}*",new string ('/',n*2-2),new string('|',n));
+            for (int i = 1; i < n-i-1; i++)
+            {
+                Console.WriteLine("*{0}*{1}*{0}*", new string('/', n * 2 - 2), new string(' ', n));
+            }
+            Console.WriteLine("{0}{1}{0}", new string('*', n * 2), new string(' ', n));
+
+        }
+    }
+
