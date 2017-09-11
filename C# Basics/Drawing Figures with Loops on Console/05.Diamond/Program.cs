7c8173e06b9a0ca912b883d9c674f35b0f2166fc
stefan0303 <tepsiev@hotmail.com>
2017-09-11 14:53:11 +0300

    .

---
+++ C# Basics/Drawing Figures with Loops on Console/05.Diamond/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine()); //Write a number to 15
+        Console.WriteLine("{0}{1}{0}", new string('.', n), new string('*', n * 3));
+        for (int row = 0; row < n - 1; row++)
+        {
+            Console.WriteLine("{0}*{1}{2}*{0}", new string('.', n - row - 1), new string('.', n * 3 + row), new string('.', row));
+        }
+        Console.WriteLine(new string('*', n * 5));
+        for (int row = 2; row < n * 2 + 2; row++)
+        {
+            Console.WriteLine("{0}*{1}*{0}", new string('.', row - 1), new string('.', n * 5 - 2 * row));
+        }
+        Console.WriteLine("{0}{1}{0}", new string('.', n * 2 + 1), new string('*', n - 2));
+    }
+}
---
+++ C# Basics/Drawing Figures with Loops on Console/07.STOP_draw/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());//Write a number to 20
+        Console.WriteLine("{0}{1}{0}", new string('.', n + 1), new string('_', n * 2 + 1));
+        {
+            for (int row = 0; row < n; row++)
+            {
+                Console.WriteLine(@"{0}//{1}\\{0}", new string('.', n - row), new string('_', n * 2 - 1 + 2 * row));
+            }
+            Console.WriteLine(@"//{0}STOP!{0}\\", new string('_', (n * 4 + 3 - 9) / 2));
+            Console.WriteLine(@"\\{0}//", new string('_', (n * 4 + 3 - 4)));
+            for (int row = 1; row < n; row++)
+            {
+                Console.WriteLine(@"{0}\\{1}//{0}", new string('.', row), new string('_', (n * 4 + 3 - 4 - row * 2)));
+            }
+
+        }
+    }
+}
