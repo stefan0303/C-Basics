35ac836b7cfd3898463c019bceadbdd181af43be
stefan0303 <tepsiev@hotmail.com>
2017-09-11 16:15:57 +0300

    new exercises

---
+++ C# Basics/Advanced_Loops_Exercises/02.Check_Prime_Number/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        bool prime = true;
+
+        if (n < 2)
+        {
+            Console.WriteLine("Not Prime");
+        }
+        else
+        {
+            for (int i = 2; i <= n - 1; i++)
+            {
+                if (n % i == 0)
+                {
+                    prime = false;
+                    break;
+                }
+            }
+            if (prime)
+            {
+                Console.WriteLine("Prime");
+            }
+            else
+            {
+                Console.WriteLine("Not Prime");
+            }
+        }
+    }
+}
