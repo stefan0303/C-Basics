3938e8b4f62f745c6c436603e612151d4abb1846
stefan0303 <tepsiev@hotmail.com>
2017-09-21 17:12:18 +0300

    .

---
+++ Advanced_Loops_Exercises/06.Max_Combination/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int first = int.Parse(Console.ReadLine());
+        int second = int.Parse(Console.ReadLine());
+        int max = int.Parse(Console.ReadLine());
+        int count = 0;
+        for (int i = first; i <= second; i++)
+        {
+            for (int j = first; j <= second; j++)
+            {
+                if (count == max)
+                {
+                    break;
+                }
+                Console.Write("<{0}-{1}>", i, j);
+                count++;
+            }
+        }
+    }
+}
---
+++ Drawing Figures with Loops on Console/11.Rocket/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine("{0}/{1}\\{0}", new string('.', (3 * n) / 2 - i - 1), new string(' ', i * 2));
+        }
+        Console.WriteLine("{0}{1}{0}", new string('.', n / 2), new string('*', n * 2));
+        for (int i = 1; i <= n * 2; i++)
+        {
+            Console.WriteLine("{0}|{1}|{0}", new string('.', n / 2), new string('\\', 3 * n - n - 2));
+        }
+        for (int i = 0; i < n / 2; i++)
+        {
+            Console.WriteLine("{0}/{1}\\{0}", new string('.', n / 2 - i), new string('*', 3 * n - n - 2 + i * 2));
+        }
+    }
+}
